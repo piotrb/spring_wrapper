e818a17502dcdc007a0c20f2fbd87cbd4c7f93b0
Piotr Banasik <piotr@jane.app>
2020-09-28 12:08:07 -0700

    Initial script

---
+++ spring_wrapper.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/piotrb/go-utils/bundle"
+	"github.com/piotrb/go-utils/utils"
+)
+
+func main() {
+	hasBundle := utils.FileExists("Gemfile")
+	isCompatibleApp := (utils.FileExists("config/application.rb") || utils.FileExists("config/spring.rb")) && utils.FileExists(".spring")
+	var hasSpringInBundle = false
+	if hasBundle {
+		var err = error(nil)
+		hasSpringInBundle, err = bundle.BundleHasE("spring")
+		if err != nil {
+			fmt.Printf("Failed Checking for Spring in Bundle:\n%s", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Printf("[bundle: %v | compatible: %v | in_bundle: %v] ... ", hasBundle, isCompatibleApp, hasSpringInBundle)
+
+	cmdParts := os.Args[1:len(os.Args)]
+
+	if len(cmdParts) < 1 {
+		fmt.Fprint(os.Stderr, "must specify command\n")
+		os.Exit(1)
+	}
+
+	if isCompatibleApp && ((hasBundle && hasSpringInBundle) || !hasBundle) {
+		fmt.Printf("Running via Spring\n")
+		utils.Backtick("bundle", "check")
+		utils.Exec(append([]string{"bundle", "exec", "spring"}, cmdParts...)...)
+	} else if hasBundle {
+		fmt.Printf("Running via Bundler\n")
+		utils.Backtick("bundle", "check")
+		utils.Exec(append([]string{"bundle", "exec"}, cmdParts...)...)
+	} else {
+		fmt.Printf("Running Directly\n")
+		utils.Exec(cmdParts...)
+	}
+}
